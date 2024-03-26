59924cdd0c4814ed66bd00f5abfd7b441921475a
Lucas Mounsey <lucas@today.design>
2024-03-26 15:01:53 +1100

    [FIX] Update event name

--- src/Revalidate.php
+++ src/Revalidate.php
@@ -95,7 +95,7 @@ class Revalidate extends Plugin
     {
         Event::on(
             Utilities::class,
-            Utilities::EVENT_REGISTER_UTILITY_TYPES,
+            Utilities::EVENT_REGISTER_UTILITIES,
             function (RegisterComponentTypesEvent $event) {
                 $event->types[] = RevalidateUtility::class;
             }
