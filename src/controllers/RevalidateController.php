980247ae8dfdc23a937cfa65faf66bc8c07aa3f6
Lucas Mounsey <lucas@today.design>
2024-09-17 07:42:05 +1000

    [FEAT] Revalidate paths/tags

--- src/controllers/RevalidateController.php
+++ src/controllers/RevalidateController.php
@@ -38,4 +38,58 @@ class RevalidateController extends Controller
 
         return $this->redirect(UrlHelper::url('utilities/'.RevalidateUtility::id()));
     }
+
+    public function actionRevalidatePaths()
+    {
+        $this->requirePostRequest();
+
+        $paths = Craft::$app->getRequest()->getBodyParam('paths');
+
+        // If no paths, add error message
+        if (!$paths) {
+            Craft::$app->getSession()->setError('No paths to revalidate');
+            return null;
+        }
+
+        // Get first item of the array, then join the arrays
+        $paths = array_map(function($path) {
+            return $path[0];
+        }, $paths);
+
+        // Deduplicate
+        $paths = array_unique($paths);
+
+        $siteUrl = Craft::$app->getRequest()->getBodyParam('siteUrl');
+
+        Craft::$app->getPlugins()->getPlugin('revalidate')->getService()->revalidate($siteUrl, ['paths' => $paths, 'tags' => []]);
+
+        return $this->redirect(UrlHelper::url('utilities/'.RevalidateUtility::id()));
+    }
+
+    public function actionRevalidateTags()
+    {
+        $this->requirePostRequest();
+
+        $tags = Craft::$app->getRequest()->getBodyParam('tags');
+
+        // If no tags, add error message
+        if (!$tags) {
+            Craft::$app->getSession()->setError('No tags to revalidate');
+            return null;
+        }
+
+        // Get first item of the array, then join the arrays
+        $tags = array_map(function($tag) {
+            return $tag[0];
+        }, $tags);
+
+        // Deduplicate
+        $tags = array_unique($tags);
+
+        $siteUrl = Craft::$app->getRequest()->getBodyParam('siteUrl');
+
+        Craft::$app->getPlugins()->getPlugin('revalidate')->getService()->revalidate($siteUrl, ['paths' => [], 'tags' => $tags]);
+
+        return $this->redirect(UrlHelper::url('utilities/'.RevalidateUtility::id()));
+    }
 }
--- src/services/RevalidateService.php
+++ src/services/RevalidateService.php
@@ -52,6 +52,31 @@ class RevalidateService extends Component
     // Check if there is a matching hook in settings
     foreach ($settings->revalidateHooks as $key=>$value) {
       if ($key === $graphqlType) {
+        // Check to see if there is a property that matches the section handle
+        if (isset($value[$element->section->handle])) {
+          $sectionConfig = $value[$element->section->handle];
+
+           // Check for `tags` property
+          if (isset($sectionConfig['tags'])) {
+            // If is array, add to tags
+            if (is_array($sectionConfig['tags'])) {
+              $tags = array_merge($tags, $sectionConfig['tags']);
+            } else {
+              $tags[] = $sectionConfig['tags'];
+            }
+          }
+
+          // Check for `paths` property
+          if (isset($sectionConfig['paths'])) {
+            // If is array, add to paths
+            if (is_array($sectionConfig['paths'])) {
+              $paths = array_merge($paths, $sectionConfig['paths']);
+            } else {
+              $paths[] = $sectionConfig['paths'];
+            }
+          }
+        }
+
         // Check for `tags` property
         if (isset($value['tags'])) {
           // If is array, add to tags
@@ -79,7 +104,9 @@ class RevalidateService extends Component
       $this->revalidate($siteUrl, [ 'paths' => $paths, 'tags' => $tags ]);
 
       if ($settings->prefetch) {
-        // Prefetch URLs
+        // Deduplicate paths
+        $paths = array_unique($paths);
+
         foreach ($paths as $path) {
           $url = $siteUrl . $path;
 
@@ -89,6 +116,7 @@ class RevalidateService extends Component
           $task = new PrefetchTask($url);
 
           Craft::$app->queue->ttr(3600);
+          Craft::$app->queue->priority(1024);
           Craft::$app->queue->push($task);
         }
       }
