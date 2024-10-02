3744c50d29bb20f73859e2e2dd0ba27fb4fe8a79
Beau Hankins <beau@today.design>
2024-10-02 13:45:16 +1000

    [FIX] Use safer migrations

--- src/migrations/Install.php
+++ src/migrations/Install.php
@@ -7,21 +7,23 @@ class Install extends Migration
 {
     public function safeUp(): bool
     {
-        $this->createTable('{{%revalidate_deployment_status}}', [
-            'id' => $this->primaryKey(),
-            'type' => $this->string()->notNull(),
-            'createdAt' => $this->dateTime()->notNull(),
-            'dateCreated' => $this->dateTime()->notNull(),
-            'dateUpdated' => $this->dateTime()->notNull(),
-            'uid' => $this->uid(),
-        ]);
+        if (!$this->db->tableExists('{{%revalidate_deployment_status}}')) {
+          $this->createTable('{{%revalidate_deployment_status}}', [
+              'id' => $this->primaryKey(),
+              'type' => $this->string()->notNull(),
+              'createdAt' => $this->dateTime()->notNull(),
+              'dateCreated' => $this->dateTime()->notNull(),
+              'dateUpdated' => $this->dateTime()->notNull(),
+              'uid' => $this->uid(),
+          ]);
+        }
 
         return true;
     }
 
     public function safeDown(): bool
     {
-        $this->dropTable('{{%revalidate_deployment_status}}');
+        $this->dropTableIfExists('{{%revalidate_deployment_status}}');
 
         return true;
     }
--- src/migrations/m240920_000000_create_deployment_status_table.php
+++ src/migrations/m240920_000000_create_deployment_status_table.php
@@ -7,18 +7,20 @@ class m240920_000000_create_deployment_status_table extends Migration
 {
     public function safeUp()
     {
-        $this->createTable('{{%revalidate_deployment_status}}', [
-            'id' => $this->primaryKey(),
-            'type' => $this->string()->notNull(),
-            'createdAt' => $this->dateTime()->notNull(),
-            'dateCreated' => $this->dateTime()->notNull(),
-            'dateUpdated' => $this->dateTime()->notNull(),
-            'uid' => $this->uid(),
-        ]);
+        if (!$this->db->tableExists('{{%revalidate_deployment_status}}')) {
+          $this->createTable('{{%revalidate_deployment_status}}', [
+              'id' => $this->primaryKey(),
+              'type' => $this->string()->notNull(),
+              'createdAt' => $this->dateTime()->notNull(),
+              'dateCreated' => $this->dateTime()->notNull(),
+              'dateUpdated' => $this->dateTime()->notNull(),
+              'uid' => $this->uid(),
+          ]);
+        }
     }
 
     public function safeDown()
     {
-        $this->dropTable('{{%revalidate_deployment_status}}');
+        $this->dropTableIfExists('{{%revalidate_deployment_status}}');
     }
 }
