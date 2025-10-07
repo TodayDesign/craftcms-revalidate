76af24845dd3980fdd82d8f06acf9a623a76a2e0
Lucas Mounsey <lucas@today.design>
2025-10-08 10:50:26 +1100

    [FEAT] Fix redirects task

--- src/services/RevalidateService.php
+++ src/services/RevalidateService.php
@@ -140,6 +140,18 @@ class RevalidateService extends Component
     }
   }
 
+  public function revalidateRedirects() {
+    $settings = $this->getSettings();
+    $tags = ['redirects'];
+    $paths = [];
+
+    // Revalidate paths and tags
+    $revalidateTask = new RevalidateTask(Craft::$app->sites->currentSite->getBaseUrl(), [ 'paths' => $paths, 'tags' => $tags ], $settings->delay);
+    Craft::$app->queue->ttr(3600);
+    Craft::$app->queue->priority(1024);
+    Craft::$app->queue->push($revalidateTask);
+  }
+
   public function revalidateAll() {
     $this->revalidate(Craft::$app->sites->currentSite->getBaseUrl(), [ 'paths' => ['/*'], 'tags' => ['site-data']]);
   }
